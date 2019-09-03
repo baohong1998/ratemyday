fcdfe63b43c71f8e4feb319a5f2f4d44bb874c55
baohong1998 <bao_mu2012@yahoo.com.vn>
2019-09-03 02:46:40 -0400

    will work on this tomorroe

--- components/authen/signup.js
+++ components/authen/signup.js
@@ -1,60 +1,71 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {Button} from 'react-native-elements'
-import styles from '../../AppStyle'
-import {createStackNavigator, createAppContainer} from 'react-navigation'
+import { Button } from 'react-native-elements';
+import styles from '../../AppStyle';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { Input } from 'react-native-elements';
 export default class SignupScreen extends Component {
-    goback = ()=>{
-        this.props.navigation.navigate('Home')
-    }
-    goNext = ()=>{
-        this.props.navigation.navigate('UserPass')
-    }
-    render(){
-        return(
-            <View style={styles.container}>
-                <Text style={styles.homeLogo}>Logo</Text>
-                <Input
-                    placeholder='First Name'
-                    inputStyle={styles.input}
-                    containerStyle={styles.inputContainer}
-                    inputContainerStyle={styles.inputWrapper}
-                />
-                <Input
-                    placeholder='Last Name'
-                    inputStyle={styles.input}
-                    containerStyle={styles.inputContainer}
-                    inputContainerStyle={styles.inputWrapper}
-                />
-                <Input
-                    placeholder='Email Address'
-                    inputStyle={styles.input}
-                    containerStyle={styles.inputContainer}
-                    inputContainerStyle={styles.inputWrapper}
-                />
-                <Input
-                    placeholder='Phone Number'
-                    inputStyle={styles.input}
-                    containerStyle={styles.inputContainer}
-                    inputContainerStyle={styles.inputWrapper}
-                />
-                <Input
-                    placeholder='Gender'
-                    inputStyle={styles.input}
-                    containerStyle={styles.inputContainer}
-                    inputContainerStyle={styles.inputWrapper}
-                />
-                <Input
-                    placeholder='Birthday'
-                    inputStyle={styles.input}
-                    containerStyle={styles.inputContainer}
-                    inputContainerStyle={styles.inputWrapper}
-                />
-               
-                <Button onPress={this.goNext} title="Next" buttonStyle={styles.loginButton}/>
-            </View>
-        )
-    }
+  state = {
+    first: '',
+    last: '',
+    email: '',
+    phone: '',
+    gender: '',
+    birthday: ''
+  };
+  goback = () => {
+    this.props.navigation.navigate('Home');
+  };
+  goNext = () => {
+    this.props.navigation.navigate('UserPass');
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.homeLogo}>Logo</Text>
+        <Input
+          placeholder="First Name"
+          inputStyle={styles.input}
+          containerStyle={styles.inputContainer}
+          inputContainerStyle={styles.inputWrapper}
+        />
+        <Input
+          placeholder="Last Name"
+          inputStyle={styles.input}
+          containerStyle={styles.inputContainer}
+          inputContainerStyle={styles.inputWrapper}
+        />
+        <Input
+          placeholder="Email Address"
+          inputStyle={styles.input}
+          containerStyle={styles.inputContainer}
+          inputContainerStyle={styles.inputWrapper}
+        />
+        <Input
+          placeholder="Phone Number"
+          inputStyle={styles.input}
+          containerStyle={styles.inputContainer}
+          inputContainerStyle={styles.inputWrapper}
+        />
+        <Input
+          placeholder="Gender"
+          inputStyle={styles.input}
+          containerStyle={styles.inputContainer}
+          inputContainerStyle={styles.inputWrapper}
+        />
+        <Input
+          placeholder="Birthday"
+          inputStyle={styles.input}
+          containerStyle={styles.inputContainer}
+          inputContainerStyle={styles.inputWrapper}
+        />
 
+        <Button
+          onPress={this.goNext}
+          title="Next"
+          buttonStyle={styles.loginButton}
+        />
+      </View>
+    );
+  }
 }
